4ed75018108facde1f64786b3e13a8144658e02d
Alexey <ale-kuchner@mail.ru>
2022-05-26 18:50:23 +0500

    version 01.00.00

--- src/main/java/LinksSuggester.java
+++ src/main/java/LinksSuggester.java
@@ -1,13 +1,38 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LinksSuggester {
 
+    private List<Suggest> suggests;
+
     public LinksSuggester(File file) throws IOException, WrongLinksFormatException {
 
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String currentLine = reader.readLine();
+        suggests = new ArrayList<>();
+        while (currentLine != null) {
+
+            String[] objs = currentLine.split("\t");
+            if (objs.length != 3) {
+                throw new WrongLinksFormatException("There should be 3 arguments per line: " + currentLine);
+            }
+            suggests.add(new Suggest(objs[0], objs[1], objs[2]));
+
+            currentLine = reader.readLine();
+        }
     }
 
     public List<Suggest> suggest(String text) {
-        return null;
+
+        var necessary = new ArrayList<Suggest>();
+
+        for (int i = 0; i < suggests.size(); i++) {
+            var suggest = suggests.get(i);
+            if (text.toLowerCase().contains(suggest.getKeyWord().toLowerCase())) {
+                necessary.add(suggest);
+            }
+        }
+        return necessary;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,19 @@
+import com.itextpdf.kernel.colors.DeviceRgb;
+import com.itextpdf.kernel.geom.Rectangle;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.action.PdfAction;
+import com.itextpdf.kernel.pdf.annot.PdfLinkAnnotation;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+import com.itextpdf.layout.Canvas;
+import com.itextpdf.layout.element.Link;
+import com.itextpdf.layout.element.Paragraph;
+
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -6,13 +21,69 @@ public class Main {
         LinksSuggester linksSuggester = new LinksSuggester(new File("data/config"));
 
         // перебираем пдфки в data/pdfs
+        var dir = new File("data/pdfs");
+        for (var fileIn : Objects.requireNonNull(dir.listFiles())) {
 
-        // для каждой пдфки создаём новую в data/converted
+            // для каждой пдфки создаём новую в data/converted
+            var doc = new PdfDocument(new PdfReader(fileIn),
+                    new PdfWriter(
+                            new File("data/converted/"
+                                    + fileIn.getName())));
 
-        // перебираем страницы pdf
+            // перебираем страницы pdf
+            List<String> pages = new ArrayList<>();
+            for (int i = 1; i <= doc.getNumberOfPages(); i++) {
+                pages.add(PdfTextExtractor.getTextFromPage(
+                        doc.getPage(i)));
+            }
 
-        // если в странице есть неиспользованные ключевые слова, создаём новую страницу за ней
+            List<String> keyWords = new ArrayList<>();
 
-        // вставляем туда рекомендуемые ссылки из конфига
+            int i = 0;
+            for (String page : pages) {
+
+                List<Suggest> suggests = linksSuggester.suggest(String.valueOf(page));
+                List<String> urls = new ArrayList<>();
+                List<String> titles = new ArrayList<>();
+                for (Suggest suggest :suggests) {
+                    if (!keyWords.contains(suggest.getKeyWord())) {
+                            urls.add(suggest.getUrl());
+                            titles.add(suggest.getTitle());
+                            keyWords.add(suggest.getKeyWord());
+                        }
+                    }
+
+
+
+                // если в странице есть использованные ключевые слова, создаём новую страницу за ней
+                i++;
+                if (titles.size() > 0) {
+                    var newPage = doc.addNewPage(
+                            pages.indexOf(page) + i + 1);
+
+                    var rect = new Rectangle(
+                            newPage.getPageSize()).moveRight(10).moveDown(10);
+                    Canvas canvas = new Canvas(newPage, rect);
+                    Paragraph paragraph = new Paragraph("Suggestions:\n")
+                            .setFontColor(new DeviceRgb(0, 0, 0))
+                            .setBackgroundColor(new DeviceRgb(0, 255, 130));
+                    paragraph.setFontSize(25);
+                    // вставляем туда рекомендуемые ссылки из конфига
+
+                    for (int j = 0; j < titles.size(); j++) {
+                        PdfLinkAnnotation annotation = new PdfLinkAnnotation(rect);
+                        PdfAction action = PdfAction.createURI(urls.get(j));
+                        annotation.setAction(action);
+                        Link link = new Link(titles.get(j), annotation);
+                        paragraph.add(link.setUnderline());
+                        paragraph.add("\n");
+                    }
+
+                    canvas.add(paragraph);
+                    canvas.close();
+                }
+            }
+            doc.close();
+        }
     }
 }
